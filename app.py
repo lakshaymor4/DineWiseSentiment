c84388ba4312309e6a72b2b8d0ec2ca468c45f8a
lakshaymor4 <117587156+lakshaymor4@users.noreply.github.com>
2025-02-27 16:43:45 +0530

    Dine Wise sentiment analyzer

---
+++ app.py
@@ -0,0 +1,53 @@
+from flask import Flask, request, jsonify
+import torch
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import torch._dynamo
+
+app = Flask(__name__)
+
+# Set device
+torch._dynamo.config.suppress_errors = True
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+model_id = "nlptown/bert-base-multilingual-uncased-sentiment"
+
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+model = AutoModelForSequenceClassification.from_pretrained(model_id, torch_dtype=torch.float16)
+
+model.to(device)
+model.eval()
+id2label = model.config.id2label
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    try:
+        data = request.get_json()
+        text = data.get("data", "")
+
+        if not text:
+            return jsonify({"error": "No text provided"}), 400
+
+        inputs = tokenizer(text, return_tensors="pt").to(device)
+
+        with torch.inference_mode():
+            outputs = model(**inputs)
+            predictions = outputs.logits.argmax(dim=-1)
+
+        rating = id2label[predictions.item()]
+        sentiment = ""
+        if(int(rating[0])>2 and int(rating[0])<4):
+            sentiment = "neutral"
+        elif(int(rating[0])>=4):
+            sentiment = "positive"
+        else:
+            sentiment = "negative"
+       
+        
+        return jsonify({"sentiment": sentiment})
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
