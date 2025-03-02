fca77e61f4141ea49cd09adfad503739f94203fa
lakshaymor4 <117587156+lakshaymor4@users.noreply.github.com>
2025-03-02 11:58:00 +0530

    updated app

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 import torch
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import torch._dynamo
+import os
 
 app = Flask(__name__)
 
@@ -50,4 +51,5 @@ def predict():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000,)
+    port = int(os.environ.get("PORT", 5000))  
+    app.run(host="0.0.0.0", port=port)
